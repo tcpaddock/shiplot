9430230472bb0abab90117e4f070e47a4db628e3
Taylor Paddock <tcpaddock@gmail.com>
2023-02-06 11:32:17 -0500

    migrate watcher and plot mover to sower pkg

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/panjf2000/ants/v2 v2.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
--- internal/sower/sower.go
+++ internal/sower/sower.go
@@ -23,20 +23,180 @@ package sower
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 
+	"github.com/fsnotify/fsnotify"
+	"github.com/panjf2000/ants/v2"
 	"github.com/tcpaddock/shiplot/internal/config"
+	"golang.org/x/exp/slog"
 )
 
 type Sower struct {
-	ctx context.Context
-	cfg *config.Config
+	ctx      context.Context
+	cancel   context.CancelFunc
+	cfg      config.Config
+	paths    *pathList
+	movePool *ants.PoolWithFunc
+	watcher  *fsnotify.Watcher
+	wg       sync.WaitGroup
 }
 
-func NewSower(ctx context.Context, cfg *config.Config) (t *Sower) {
+func NewSower(ctx context.Context, cfg config.Config) (t *Sower, err error) {
 	t = new(Sower)
 
-	t.ctx = ctx
+	t.ctx, t.cancel = context.WithCancel(ctx)
 	t.cfg = cfg
 
-	return t
+	// Fill list of available destination paths
+	t.paths = new(pathList)
+	t.paths.Populate(t.cfg.DestinationPaths)
+
+	// Create worker pool for moving plots
+	t.movePool, err = ants.NewPoolWithFunc(t.paths.AvailableCount(), func(i interface{}) {
+		t.movePlot(i)
+		t.wg.Done()
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (s *Sower) Run() (err error) {
+	// Create filesystem watcher
+	s.watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+
+	// Start running watcher
+	s.wg.Add(1)
+	go s.runLoop()
+
+	// Add staging path to watcher
+	slog.Default().Info(fmt.Sprintf("Starting watcher on %s", s.cfg.StagingPath))
+	err = s.watcher.Add(s.cfg.StagingPath)
+	if err != nil {
+		s.Close()
+		return err
+	}
+
+	return nil
+}
+
+func (s *Sower) Close() {
+	s.cancel()
+	s.wg.Wait()
+}
+
+func (s *Sower) runLoop() {
+	defer s.wg.Done()
+	for {
+		select {
+		// Read from Errors
+		case err, ok := <-s.watcher.Errors:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called)
+				return
+			}
+			slog.Default().Error("File watcher error", err)
+		// Read from Events
+		case e, ok := <-s.watcher.Events:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called)
+				return
+			}
+
+			if e.Op.Has(fsnotify.Create) && strings.HasSuffix(e.Name, ".plot") {
+				s.wg.Add(1)
+				s.movePool.Invoke(e.Name)
+			}
+		// Read from context for closing
+		case <-s.ctx.Done():
+			s.watcher.Close()
+			s.movePool.Release()
+			return
+		}
+	}
+}
+
+func (s *Sower) movePlot(i interface{}) {
+	var (
+		srcFullName    = i.(string)
+		srcName        = filepath.Base(srcFullName)
+		index, dstPath = s.paths.FirstAvailable()
+		dstDir         = dstPath.name
+		dstFullName    = filepath.Join(dstDir, srcName)
+	)
+
+	slog.Default().Info(fmt.Sprintf("Moving %s to %s", srcName, dstDir))
+
+	// Open source file
+	src, err := os.Open(srcFullName)
+	if err != nil {
+		src.Close()
+		s.paths.Update(index, true)
+		slog.Default().Error(fmt.Sprintf("Failed to open %s", srcFullName), err)
+		return
+	}
+
+	// Open destination file
+	dst, err := os.Create(dstFullName + ".tmp")
+	if err != nil {
+		src.Close()
+		dst.Close()
+		s.paths.Update(index, true)
+		slog.Default().Error(fmt.Sprintf("Failed to create temp file %s", dstFullName+".tmp"), err)
+		return
+	}
+
+	start := time.Now()
+
+	// Copy plot to temporary file
+	written, err := io.Copy(dst, src)
+	if err != nil {
+		src.Close()
+		dst.Close()
+		s.paths.Update(index, true)
+		slog.Default().Error(fmt.Sprintf("Failed to copy %s to %s", srcFullName, dstFullName+".tmp"), err)
+		return
+	}
+
+	// Rename temporary file
+	err = os.Rename(dstFullName+".tmp", dstFullName)
+	if err != nil {
+		src.Close()
+		dst.Close()
+		s.paths.Update(index, true)
+		slog.Default().Error(fmt.Sprintf("Failed to rename %s to %s", dstFullName+".tmp", dstFullName), err)
+		return
+	}
+
+	// Close source file
+	err = src.Close()
+	if err != nil {
+		s.paths.Update(index, true)
+		slog.Default().Error(fmt.Sprintf("Failed to close %s", srcFullName), err)
+		return
+	}
+
+	// Close destination file
+	err = dst.Close()
+	if err != nil {
+		s.paths.Update(index, true)
+		slog.Default().Error(fmt.Sprintf("Failed to close %s", dstFullName), err)
+		return
+	}
+
+	duration := time.Since(start)
+
+	// Update available paths
+	s.paths.Update(index, true)
+
+	slog.Default().Info(fmt.Sprintf("Moved %s to %s", srcName, dstDir), slog.Int64("written", written), slog.Duration("time", duration))
 }
